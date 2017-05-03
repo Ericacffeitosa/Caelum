cfac2c9a6027765a76ceb6c9fca4494ff53011df
Ericacffeitosa <ericacffeitosa@gmail.com>
2017-05-03 00:25:27 -0300

    exercicio tres

--- Caelum/src/exerciciotres/Aluno.java
+++ Caelum/src/exerciciotres/Aluno.java
@@ -1,5 +1,5 @@
 package exerciciotres;
-import  java.lang.Comparable;
+import java.lang.Comparable;
 
 public class Aluno implements Comparable<Aluno> {
 	
@@ -8,27 +8,27 @@ public class Aluno implements Comparable<Aluno> {
 	
 	public Aluno (String nome, int problemasResolvidos) {
 		this.nome = nome;
-	    this.problemasResolvidos = problemasResolvidos;
+		this.problemasResolvidos = problemasResolvidos;
 	}
 	
 //	Método que compara quant. de problemas resolvidos.
 	@Override
 	public int compareTo(Aluno outroAluno) {
-        if (this.problemasResolvidos > outroAluno.problemasResolvidos) {
-            return -1;
-        }
-        if (this.problemasResolvidos < outroAluno.problemasResolvidos) {
-            return 1;
-        }
-        
-        return nome.compareTo(outroAluno.nome);
+		if (this.problemasResolvidos > outroAluno.problemasResolvidos) {
+			return -1;
+		}
+		if (this.problemasResolvidos < outroAluno.problemasResolvidos) {
+			return 1;
+		}
+		
+		return nome.compareTo(outroAluno.nome);
 	}
 		
-//  Getters and Setters
-    public String getNome() {
+	//Getters and Setters
+	public String getNome() {
 		return nome;
 	}
-    public void setNome(String nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	public int getProblemasResolvidos() {
@@ -37,6 +37,4 @@ public class Aluno implements Comparable<Aluno> {
 	public void setProblemasResolvidos(int problemasResolvidos) {
 		this.problemasResolvidos = problemasResolvidos;
 	}
-	
-	
 }	
